7b2418e8ed0a249eb34f5519cffcdbe265a83f32
xqy2006 <48169260+xqy2006@users.noreply.github.com>
2023-07-08 20:53:04 +0800

    Update index.ts

--- frontend/src/stores/modules/chat/index.ts
+++ frontend/src/stores/modules/chat/index.ts
@@ -28,7 +28,7 @@ export const useChatStore = defineStore(
         label: 'Bing 官方',
       },
       {
-        baseUrl: 'https://sydney.vcanbb.chat',
+        baseUrl: 'https://bingai.xuqinyang.top',
         label: 'Cloudflare',
       },
       {
